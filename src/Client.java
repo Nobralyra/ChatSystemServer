a285c606be9da97a52cde92e279b9ffde6ded0bb
Nobralyra <signees96@gmail.com>
2020-02-19 11:37:02 +0100

    Added come of the commands

--- src/Client.java
+++ src/Client.java
@@ -1,53 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Client
 {
-    private int serverPort = 9191;
-    private String serverIP = "127.0.0.1";
-
-    public void ClienConnect()
+    public void ClientConnect()
     {
         try
         {
-            Socket socket = new Socket(serverIP, serverPort);
+            /**
+             * Local variable:
+             * Clients socket to communicate with the server
+             */
+            Socket socket;
+            ClientServerHandler clientServerHandler;
 
-            ServerConnection serverConnection = new ServerConnection(socket);
+            BufferedReader keyboard;
+            PrintWriter output;
 
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            while(true)
+            {
+                keyboard = new BufferedReader(new InputStreamReader(System.in));
+                String x = keyboard.readLine();
+                String connectionData = x.substring(x.indexOf(", ", 2) + 2);
+                try
+                {
+                    String serverIP = connectionData.substring(0,connectionData.indexOf(":"));
+                    String serverPort = connectionData.substring(connectionData.indexOf(":") + 1);
+                    socket = new Socket(serverIP, Integer.parseInt(serverPort));
+                }
+                catch (Exception e)
+                {
+                    System.err.println("Failed to connect! Try again");
+                    continue;
+                }
 
+                output = new PrintWriter(socket.getOutputStream(), true);
+                output.println(x);
+                break;
+            }
+
+            clientServerHandler = new ClientServerHandler(socket);
             /**
              * Only one thread is needed in the client class because the client do not need to connect to multiple servers
              */
-            new Thread(serverConnection).start();
+            new Thread(clientServerHandler).start();
 
             while(true)
             {
                 String command = keyboard.readLine();
 
                 /**
-                 * JOIN <<user_name>>, <<server_ip>>:<<server_port>>
-                 * DATA <<user_name>>: <<free text…>>
-                 * IMAV
-                 * QUIT
-                 */
-                /**
-                 * Maybe a switch case instead
-                 */
-                /**
                  * The Client must send a Quit message when it is closing.
                  */
-                if (command.equals("QUIT"))
+                switch (command.substring(0,4))
                 {
+                    case "QUIT":
+                        output.println("QUIT");
+                        socket.close();
+                        System.exit(1);
+                        break;
+                    case "JOIN":
+                        break;
+                    case "DATA":
+                    case "IMAV":
+                    case "LIST":
+                        output.println(command); //Is this still needed?
                     break;
+                    default:
+                        System.err.println("Command Error - No such command exists!");
+                        break;
                 }
             }
-            socket.close(); //Noget galt her
-            //System.exit(0); //should maybe not be used
         }
         catch (IOException e)
         {
