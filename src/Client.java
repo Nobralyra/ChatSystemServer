c2c8a1b56f595b5d50601f93c4ec3e5982e6ffbf
Nobralyra <signees96@gmail.com>
2020-02-26 17:42:33 +0100

    Added comments

--- src/Client.java
+++ src/Client.java
@@ -14,20 +14,40 @@ public class Client
              * Clients socket to communicate with the server
              */
             Socket socket;
+
             ClientServerHandler clientServerHandler;
 
             BufferedReader keyboard;
             PrintWriter output;
 
+            /**
+             * Connection to the server
+             */
             while(true)
             {
+                /**
+                 * Reads what the user has written on the keyboard
+                 */
                 keyboard = new BufferedReader(new InputStreamReader(System.in));
                 String x = keyboard.readLine();
+                /**
+                 * Only want was is after the ", " like JOIN a, 127.0.0.1:9191 to 127.0.0.1:9191
+                 */
                 String connectionData = x.substring(x.indexOf(", ", 2) + 2);
                 try
                 {
+                    /**
+                     * Now we only want the first bit of the connectionDato that is 127.0.0.1
+                     */
                     String serverIP = connectionData.substring(0,connectionData.indexOf(":"));
+                    /**
+                     * Now we want the last bit of the connectionData that is 9191.
+                     * Because the ":" is still there we need say it stands on index 1
+                     */
                     String serverPort = connectionData.substring(connectionData.indexOf(":") + 1);
+                    /**
+                     * Now the information can be put into socket
+                     */
                     socket = new Socket(serverIP, Integer.parseInt(serverPort));
                 }
                 catch (Exception e)
@@ -36,17 +56,26 @@ public class Client
                     continue;
                 }
 
+                /**
+                 * Sends the information to where the socket is pointing to
+                 */
                 output = new PrintWriter(socket.getOutputStream(), true);
                 output.println(x);
                 break;
             }
 
+            /**
+             * The constructor from ClientServerHandler now gets the information about where the communication from the server and client is
+             */
             clientServerHandler = new ClientServerHandler(socket);
             /**
              * Only one thread is needed in the client class because the client do not need to connect to multiple servers
              */
             new Thread(clientServerHandler).start();
 
+            /**
+             * Sends a heartbeat alive every 60 seconds
+             */
             Timer timer = new Timer();
             timer.scheduleAtFixedRate(new TimerTask()
             {
@@ -57,28 +86,44 @@ public class Client
                 }
             }, 60*1000, 60*1000);
 
+            /**
+             *  The while loop should run forever until something change it to false
+             */
             while(true)
             {
                 String command = keyboard.readLine();
 
                 /**
-                 * The Client must send a Quit message when it is closing.
+                 * Switch on the 4 first charaters
                  */
+
                 switch (command.substring(0,4))
                 {
                     case "QUIT":
+                        /**
+                         * The Client must send a Quit message when it is closing.
+                         * Clients system then exit
+                         */
                         output.println("QUIT");
                         System.exit(1);
                         break;
 
+                        /** Do not think this is needed
                     case "JOIN":
                         break;
+                         */
                     case "DATA":
                     case "LIST":
+                        /**
+                         * Sends a command to the server asking to see the list of active users
+                         */
                         output.println(command);
                     break;
                     default:
-                        System.err.println("Command Error - No such command exists!");
+                        /**
+                         * If the request from the client do now match any of the cases they get an error message
+                         */
+                        System.err.println("J_ER 2: Unknown Command - No such command exists!");
                         break;
                 }
             }
